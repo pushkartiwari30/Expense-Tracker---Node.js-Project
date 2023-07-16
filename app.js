51aa0a8b9acfa3e545ace0198da3a46431c7af7a
pushkartiwari30 <pushkartiwari3001@gmail.com>
2023-07-17 01:28:39 +0530

    data sent to DB /t3

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const sequelize = require('./util/database');
+const User = require('./models/user');
+
+
 app.use(cors());
+app.use(bodyParser.json({ extended: false }));
 
-
-app.post('/user/signup', (req,res)=>{
-    console.log("reqesut received");
-    res.send('OK');
+app.post('/user/signup', async (req, res) => {
+    try {
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const data = await User.create({ name: name, email: email, password: password })
+        res.status(201).json({ data: data });
+    }
+    catch (err) {
+        //const error = err.parent.sqlMessage;
+        res.status(403).json({
+            error: err
+        })
+    }
 });
-// app.use((req, res, next)=>{
-//     console.log("In the middleware!");
-// }) 
 
-app.listen(3000);
+
+sequelize.sync()
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
--- dom.js
+++ dom.js
@@ -1,23 +1,30 @@
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const errorElement = document.getElementById("error-message");
 
-signupForm.addEventListener('submit', (event)=>{
+signupForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const userObj = {
-        name : name.value,
-        email : email.value,
-        password : password.value
+        name: name.value,
+        email: email.value,
+        password: password.value
     }
     console.log(userObj);
-    
+
     // We will do a post request 
     axios.post("http://localhost:3000/user/signup", userObj)
-        .then((res)=>{
+        .then((res) => {
             console.log(res);
+            // form reset 
+
         })
-        .catch((err)=>{
-            console.log(err);
+        .catch((error) => {
+            console.log(error);
+            //addinf this error to front end 
+            const errorMessage = error.message;
+            console.log(errorMessage);
+            errorElement.textContent = errorMessage;
         })
 
 })
