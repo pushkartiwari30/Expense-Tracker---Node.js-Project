bb1a7bc31d8ea61cb2504442021b8d04e50d583a
pushkartiwari30 <pushkartiwari3001@gmail.com>
2023-08-31 22:52:56 +0530

    mysql data into .env

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense', 'root', process.env.MYSQL_PASSWORD, {
+const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
     dialect: 'mysql',
     host: 'localhost',
     timezone: 'Asia/Kolkata'
